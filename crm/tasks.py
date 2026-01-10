9362f8bf7020c962ec6d4d7b13da94e8d4fd81af
dorphines <dorphines@gmail.com>
2026-01-10 16:12:28 +0300

    added "/tmp/crm_report_log.txt" to tasks.py

--- crm/tasks.py
+++ crm/tasks.py
@@ -38,7 +38,7 @@ def generate_crm_report():
     result = schema.execute(query_full)
     
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_file_path = '/tmp/crmreportlog.txt'
+    log_file_path = '/tmp/crm_report_log.txt'
     
     if result.errors:
         log_message = f"{timestamp} - Report generation failed: {result.errors}\n"
